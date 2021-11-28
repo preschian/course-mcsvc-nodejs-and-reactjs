19e75b40e97f85fb468e368ac1bfd92a22985a09
Preschian Febryantara <preschian27@gmail.com>
2021-11-29 01:03:01 +0700

    store events

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,25 +5,32 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post("/events", (req, res) => {
   const event = req.body;
 
+  events.push(event);
+
   axios.post("http://localhost:4000/events", event).catch((err) => {
-    console.log("error on port: 4000", err.message);
+    console.log(err.message);
   });
   axios.post("http://localhost:4001/events", event).catch((err) => {
-    console.log("error on port: 4001", err.message);
+    console.log(err.message);
   });
   axios.post("http://localhost:4002/events", event).catch((err) => {
-    console.log("error on port: 4002", err.message);
+    console.log(err.message);
   });
   axios.post("http://localhost:4003/events", event).catch((err) => {
-    console.log("error on port: 4003", err.message);
+    console.log(err.message);
   });
-
   res.send({ status: "OK" });
 });
 
+app.get("/events", (req, res) => {
+  res.send(events);
+});
+
 app.listen(4005, () => {
-  console.log("event-bus service running on 4005");
+  console.log("Listening on 4005");
 });
