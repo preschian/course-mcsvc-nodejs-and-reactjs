708e621575caed8b20fbde5f0272d97b2d446980
Preschian Febryantara <preschian27@gmail.com>
2021-11-29 00:24:15 +0700

    45 add comment moderation

--- moderation/index.js
+++ moderation/index.js
@@ -5,7 +5,21 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
-app.post("/events", (req, res) => {});
+app.post("/events", async (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "CommentCreated") {
+    const status = data.content.includes("orange") ? "rejected" : "approved";
+
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentModerated",
+      data: {
+        ...data,
+        status,
+      },
+    });
+  }
+});
 
 app.listen(4003, () => {
   console.log("moderation-service running on 4003");
