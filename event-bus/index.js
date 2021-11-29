3b215a8bd1a1053f0726a5b0cef692d55b30f7ea
Preschian Febryantara <preschian27@gmail.com>
2021-11-29 15:01:21 +0700

    change localhost to clusterip

--- event-bus/index.js
+++ event-bus/index.js
@@ -12,18 +12,20 @@ app.post("/events", (req, res) => {
 
   events.push(event);
 
-  axios.post("http://localhost:4000/events", event).catch((err) => {
-    console.log(err.message);
-  });
-  axios.post("http://localhost:4001/events", event).catch((err) => {
-    console.log(err.message);
-  });
-  axios.post("http://localhost:4002/events", event).catch((err) => {
-    console.log(err.message);
-  });
-  axios.post("http://localhost:4003/events", event).catch((err) => {
-    console.log(err.message);
-  });
+  axios
+    .post("http://posts-clusterip-service:4000/events", event)
+    .catch((err) => {
+      console.log(err.message);
+    });
+  // axios.post("http://localhost:4001/events", event).catch((err) => {
+  //   console.log(err.message);
+  // });
+  // axios.post("http://localhost:4002/events", event).catch((err) => {
+  //   console.log(err.message);
+  // });
+  // axios.post("http://localhost:4003/events", event).catch((err) => {
+  //   console.log(err.message);
+  // });
   res.send({ status: "OK" });
 });
 
