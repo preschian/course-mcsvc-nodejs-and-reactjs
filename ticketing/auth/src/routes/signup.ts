4e90935a44c14f4fd4fc0421d140b03c2eab1d2e
Preschian Febryantara <preschian27@gmail.com>
2021-11-29 17:51:51 +0700

    add reasons

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -16,16 +16,15 @@ router.post(
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      throw new Error("Invalid email or password");
+      // This is javascript land (not ts)
+      const error = new Error("Invalid email or password");
+      error.reasons = errors.array();
+      throw error;
     }
-    const { email, password } = req.body;
-
     console.log("Creating a user...");
     throw new Error("Error connecting to database");
 
     res.send({});
-
-    // new User({ email, password })
   }
 );
 
