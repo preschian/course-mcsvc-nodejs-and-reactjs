8d500e2d621dc39c854dfd27047671d51fc7653a
Preschian Febryantara <preschian27@gmail.com>
2021-11-30 16:29:26 +0700

    add axios and call signup api

--- ticketing/client/pages/auth/signup.js
+++ ticketing/client/pages/auth/signup.js
@@ -1,17 +1,23 @@
 import { useState } from "react";
+import axios from "axios";
 
 export default () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const onSubmit = (event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
 
-    console.log({ email, password });
+    const response = await axios.post("/api/users/signup", {
+      email,
+      password,
+    });
+
+    console.log(response.data);
   };
 
   return (
-    <form onSubmit={onSubmit} className="container">
+    <form onSubmit={onSubmit}>
       <h1>Sign Up</h1>
       <div className="form-group">
         <label>Email Address</label>
