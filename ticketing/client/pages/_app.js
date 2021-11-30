4467ec29c20d56e79b7adfaebc969006a5456f99
Preschian Febryantara <preschian27@gmail.com>
2021-11-30 23:45:22 +0700

    multiple getInitialProps

--- ticketing/client/pages/_app.js
+++ ticketing/client/pages/_app.js
@@ -1,6 +1,7 @@
 import "bootstrap/dist/css/bootstrap.css";
+import buildClient from "../api/build-client";
 
-export default ({ Component, pageProps }) => {
+const AppComponent = ({ Component, pageProps }) => {
   return (
     <div>
       <h1>Header!</h1>
@@ -8,3 +9,19 @@ export default ({ Component, pageProps }) => {
     </div>
   );
 };
+
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get("/api/users/currentuser");
+
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  console.log(pageProps);
+
+  return data;
+};
+
+export default AppComponent;
