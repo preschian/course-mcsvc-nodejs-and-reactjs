fff81293b49e455ef16bc451ad691a27c9d97a50
Preschian Febryantara <preschian27@gmail.com>
2021-11-30 09:48:26 +0700

    connect to mongodb

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
+import mongoose, { mongo } from "mongoose";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -23,6 +24,17 @@ app.all("*", async (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000!!!!!!!!");
-});
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("connected to mongodb");
+  } catch (error) {
+    console.log(error);
+  }
+
+  app.listen(3000, () => {
+    console.log("Listening on port 3000!!!!!!!!");
+  });
+};
+
+start();
