d5ace369e2bd92b05ad68e694a65984051bd4842
Preschian Febryantara <preschian27@gmail.com>
2021-12-01 16:03:41 +0700

    add routes/show

---
+++ ticketing/tickets/src/routes/show.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from "express";
+import { NotFoundError } from "@pf-tickets/common";
+import { Ticket } from "../models/ticket";
+
+const router = express.Router();
+
+router.get("/api/tickets/:id", async (req: Request, res: Response) => {
+  const ticket = await Ticket.findById(req.params.id);
+
+  console.log(ticket);
+
+  if (!ticket) {
+    throw new NotFoundError();
+  }
+
+  res.send(ticket);
+});
+
+export { router as showTicketRouter };
