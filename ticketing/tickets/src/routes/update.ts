c9cb0298f3eaf1c4b94a6cbb9f38518557ef564e
Preschian Febryantara <preschian27@gmail.com>
2021-12-01 16:17:16 +0700

    add update ticket

---
+++ ticketing/tickets/src/routes/update.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from "express";
+import { NotFoundError, requireAuth } from "@pf-tickets/common";
+import { Ticket } from "../models/ticket";
+
+const router = express.Router();
+
+router.put(
+  "/api/tickets/:id",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const ticket = await Ticket.findById(req.params.id);
+
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    res.send(ticket);
+  }
+);
+
+export { router as updateTicketRouter };
