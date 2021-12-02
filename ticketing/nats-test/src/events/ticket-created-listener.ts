fb36ffa8e9759f46492b3743d4d80216a3634352
Preschian Febryantara <preschian27@gmail.com>
2021-12-02 15:02:05 +0700

    date event

--- ticketing/nats-test/src/events/ticket-created-listener.ts
+++ ticketing/nats-test/src/events/ticket-created-listener.ts
@@ -5,13 +5,15 @@ import { Subjects } from "./subjects";
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   readonly subject = Subjects.TicketCreated;
+
   queueGroupName = "payments-service";
 
-  onMessage(data: any, msg: Message) {
+  onMessage(data: TicketCreatedEvent["data"], msg: Message) {
     console.log("Event data!", data);
 
-    console.log(data.name);
-    console.log(data.cost);
+    console.log(data.id);
+    console.log(data.title);
+    console.log(data.price);
 
     msg.ack();
   }
