0f25b940c37690bb44bb74d125a7c50f63313422
Preschian Febryantara <preschian27@gmail.com>
2021-12-03 08:53:23 +0700

    graceful exit

--- ticketing/nats-test/src/events/ticket-created-listener.ts
+++ ticketing/nats-test/src/events/ticket-created-listener.ts
@@ -5,7 +5,6 @@ import { Subjects } from "./subjects";
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   readonly subject = Subjects.TicketCreated;
-
   queueGroupName = "payments-service";
 
   onMessage(data: TicketCreatedEvent["data"], msg: Message) {
