7e78518bcf33289b83bc5526288199965c4cf9fb
Preschian Febryantara <preschian27@gmail.com>
2021-12-03 14:39:53 +0700

    add existingOrder

--- ticketing/orders/src/routes/new.ts
+++ ticketing/orders/src/routes/new.ts
@@ -4,6 +4,8 @@ import {
   requireAuth,
   validateRequest,
   NotFoundError,
+  OrderStatus,
+  BadRequestError,
 } from "@pf-tickets/common";
 import { body } from "express-validator";
 import { Ticket } from "../models/ticket";
@@ -32,6 +34,23 @@ router.post(
     }
 
     // Make sure that this ticket is not already reserved
+    // Run query to look at all orders.  Find an order where the ticket
+    // is the ticket we just found *and* the orders status is *not* cancelled.
+    // If we find an order from that means the ticket *is* reserved
+    const existingOrder = await Order.findOne({
+      ticket: ticket,
+      status: {
+        $in: [
+          OrderStatus.Created,
+          OrderStatus.AwaitingPayment,
+          OrderStatus.Complete,
+        ],
+      },
+    });
+
+    if (existingOrder) {
+      throw new BadRequestError("Ticket is already reserved");
+    }
 
     // Calculate an expiration date for this order
 
