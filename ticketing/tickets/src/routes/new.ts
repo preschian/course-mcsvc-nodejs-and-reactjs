d7f01c2fb1e030f06c691ab44a5b5749120caf5e
Preschian Febryantara <preschian27@gmail.com>
2021-12-05 14:04:02 +0700

    add version on tickets

--- ticketing/tickets/src/routes/new.ts
+++ ticketing/tickets/src/routes/new.ts
@@ -1,20 +1,20 @@
-import express, { Request, Response } from "express";
-import { body } from "express-validator";
-import { requireAuth, validateRequest } from "@pf-tickets/common";
-import { Ticket } from "../models/ticket";
-import { TicketCreatedPublisher } from "../events/publishers/ticket-created-publisher";
-import { natsWrapper } from "../nats-wrapper";
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { requireAuth, validateRequest } from '@pf-tickets/common';
+import { Ticket } from '../models/ticket';
+import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
 router.post(
-  "/api/tickets",
+  '/api/tickets',
   requireAuth,
   [
-    body("title").not().isEmpty().withMessage("Title is required"),
-    body("price")
+    body('title').not().isEmpty().withMessage('Title is required'),
+    body('price')
       .isFloat({ gt: 0 })
-      .withMessage("Price must be greater than 0"),
+      .withMessage('Price must be greater than 0'),
   ],
   validateRequest,
   async (req: Request, res: Response) => {
@@ -26,11 +26,12 @@ router.post(
       userId: req.currentUser!.id,
     });
     await ticket.save();
-    await new TicketCreatedPublisher(natsWrapper.client).publish({
+    new TicketCreatedPublisher(natsWrapper.client).publish({
       id: ticket.id,
       title: ticket.title,
       price: ticket.price,
       userId: ticket.userId,
+      version: ticket.version,
     });
 
     res.status(201).send(ticket);
