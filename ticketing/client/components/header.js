33a8c8b96211f6b15e4663d3e8aa82c8db4d26ca
Preschian Febryantara <preschian27@gmail.com>
2021-12-05 18:35:58 +0700

    singlequote

--- ticketing/client/components/header.js
+++ ticketing/client/components/header.js
@@ -1,10 +1,10 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 export default ({ currentUser }) => {
   const links = [
-    !currentUser && { label: "Sign Up", href: "/auth/signup" },
-    !currentUser && { label: "Sign In", href: "/auth/signin" },
-    currentUser && { label: "Sign Out", href: "/auth/signout" },
+    !currentUser && { label: 'Sign Up', href: '/auth/signup' },
+    !currentUser && { label: 'Sign In', href: '/auth/signin' },
+    currentUser && { label: 'Sign Out', href: '/auth/signout' },
   ]
     .filter((linkConfig) => linkConfig)
     .map(({ label, href }) => {
