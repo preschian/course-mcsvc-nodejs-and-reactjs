5cd941b5518a033a8be2f7540f49ec9d52137ce1
Preschian Febryantara <preschian27@gmail.com>
2021-12-06 10:37:59 +0700

    add jest.setTimeout(50000)

--- ticketing/auth/src/test/setup.ts
+++ ticketing/auth/src/test/setup.ts
@@ -1,8 +1,10 @@
-import { MongoMemoryServer } from "mongodb-memory-server";
-import mongoose from "mongoose";
-import request from "supertest";
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import request from 'supertest';
 
-import { app } from "../app";
+import { app } from '../app';
+
+jest.setTimeout(50000);
 
 declare global {
   var signin: () => Promise<string[]>;
@@ -10,7 +12,7 @@ declare global {
 
 let mongo: any;
 beforeAll(async () => {
-  process.env.JWT_KEY = "asdfg";
+  process.env.JWT_KEY = 'asdfg';
 
   mongo = await MongoMemoryServer.create();
   const mongoUri = mongo.getUri();
@@ -32,15 +34,15 @@ afterAll(async () => {
 });
 
 global.signin = async () => {
-  const email = "test@test.com";
-  const password = "password";
+  const email = 'test@test.com';
+  const password = 'password';
 
   const response = await request(app)
-    .post("/api/users/signup")
+    .post('/api/users/signup')
     .send({ email, password })
     .expect(201);
 
-  const cookie = response.get("Set-Cookie");
+  const cookie = response.get('Set-Cookie');
 
   return cookie;
 };
